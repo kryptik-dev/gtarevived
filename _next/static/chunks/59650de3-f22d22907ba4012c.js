40f4e14407e35f9003d8a4de92fb79872c27e73d
Amaan Dildar <rhythmnova@outlook.com>
2026-03-14 13:44:37 +0200

    UPDATE LANGAUGES

---
+++ _next/static/chunks/59650de3-f22d22907ba4012c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[150],{4549:(t,r,e)=>{e.d(r,{zrT:()=>o});var n=e(4436);function o(t){return(0,n.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"m6 9 6 6 6-6"},child:[]}]})(t)}}}]);
